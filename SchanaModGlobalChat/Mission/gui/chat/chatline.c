520c779c7fd1ec9fd2e068b33604bd968e81f1bd
Nathaniel Schaaf <schaafna@gmail.com>
2020-06-02 09:35:50 +0200

    Improve readability

---
+++ SchanaModGlobalChat/Mission/gui/chat/chatline.c
@@ -0,0 +1,8 @@
+modded class ChatLine {
+    private const int GAME_TEXT_COLOUR = ARGB (255, 0xff, 0xd5, 0x4f);
+
+    void ChatLine (Widget root_widget) {
+        m_NameWidget.SetShadow (10);
+        m_TextWidget.SetShadow (10);
+    }
+}
