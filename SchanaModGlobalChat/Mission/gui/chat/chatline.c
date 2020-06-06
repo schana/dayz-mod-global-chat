078f7961c4b80bd0d44403a291c80917771df340
Nathaniel Schaaf <schaafna@gmail.com>
2020-06-07 01:01:37 +0200

    Use a custom layout, increasing font size

--- SchanaModGlobalChat/Mission/gui/chat/chatline.c
+++ SchanaModGlobalChat/Mission/gui/chat/chatline.c
@@ -2,7 +2,9 @@ modded class ChatLine {
     private const int GAME_TEXT_COLOUR = ARGB (255, 0xff, 0xd5, 0x4f);
 
     void ChatLine (Widget root_widget) {
-        m_NameWidget.SetShadow (10);
-        m_TextWidget.SetShadow (10);
+        m_RootWidget = GetGame ().GetWorkspace ().CreateWidgets ("SchanaModGlobalChat/GUI/layouts/chatline.layout", root_widget);
+
+        m_NameWidget = TextWidget.Cast (m_RootWidget.FindAnyWidget ("ChatItemSenderWidget"));
+        m_TextWidget = TextWidget.Cast (m_RootWidget.FindAnyWidget ("ChatItemTextWidget"));
     }
 }
