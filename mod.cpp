f38ecdf4daac70c69d090a4f410ee8768e9e6940
Nathaniel Schaaf <schaafna@gmail.com>
2020-06-11 10:32:05 +0200

    Add icon

---
+++ mod.cpp
@@ -0,0 +1,10 @@
+name = "SchanaModGlobalChat";
+picture = "SchanaModGlobalChat/GUI/textures/Carim_small.edds";
+logo = "SchanaModGlobalChat/GUI/textures/Carim_small.edds";
+logoSmall = "SchanaModGlobalChat/GUI/textures/Carim_small.edds";
+logoOver = "SchanaModGlobalChat/GUI/textures/Carim_small.edds";
+tooltip = "SchanaModGlobalChat";
+overview = "Open Source global chat mod for DayZ";
+action = "https://schana.github.io/carim/";
+author = "cnofafva";
+authorID = "76561198045960503";
