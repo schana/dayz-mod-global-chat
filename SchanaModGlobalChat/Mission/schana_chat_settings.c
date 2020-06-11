44af05c635eabeb5f3ae26524be2f5381358a9cc
Nathaniel Schaaf <schaafna@gmail.com>
2020-06-11 10:34:57 +0200

    Add alert color and update to defaults if not present in config

--- SchanaModGlobalChat/Mission/schana_chat_settings.c
+++ SchanaModGlobalChat/Mission/schana_chat_settings.c
@@ -1,11 +1,17 @@
 class SchanaModGlobalChatSettings {
     private static string DIR = "$profile:SchanaModGlobalChat";
     private static string PATH = DIR + "\\config.json";
+    private static const int DEFAULT_SIZE = 20;
+    private static const int DEFAULT_COLOR_DIRECT = 0xffffff; // white
+    private static const int DEFAULT_COLOR_GLOBAL = 0xffd54f; // amber 300
+    private static const int DEFAULT_COLOR_SERVER = 0x4fc3f7; // light blue 300
+    private static const int DEFAULT_COLOR_ALERT = 0xff8a65; // deep orange 300
 
-    private int size = 20;
-    private int color_direct = 0xffffff;
-    private int color_global = 0xffd54f;
-    private int color_server = 0x4fc3f7;
+    private int size = DEFAULT_SIZE;
+    private int color_direct = DEFAULT_COLOR_DIRECT;
+    private int color_global = DEFAULT_COLOR_GLOBAL;
+    private int color_server = DEFAULT_COLOR_SERVER;
+    private int color_alert = DEFAULT_COLOR_ALERT;
 
     int GetSize () {
         return size;
@@ -28,6 +34,10 @@ class SchanaModGlobalChatSettings {
         return GetColor (color_server);
     }
 
+    int GetColorAlert () {
+        return GetColor (color_alert);
+    }
+
     private int GetColor (int input_color) {
         return 0xff000000 | input_color;
     }
@@ -48,7 +58,27 @@ class SchanaModGlobalChatSettings {
             JsonFileLoader<SchanaModGlobalChatSettings>.JsonLoadFile (PATH, settings);
         }
 
+        LoadDefaultsIfMissing (settings);
+
         return settings;
+    }
+
+    static void LoadDefaultsIfMissing (SchanaModGlobalChatSettings settings) {
+        if (settings.size == 0) {
+            settings.size = DEFAULT_SIZE;
+        }
+        if (settings.color_direct == 0) {
+            settings.color_direct = DEFAULT_COLOR_DIRECT;
+        }
+        if (settings.color_global == 0) {
+            settings.color_global = DEFAULT_COLOR_GLOBAL;
+        }
+        if (settings.color_server == 0) {
+            settings.color_server = DEFAULT_COLOR_SERVER;
+        }
+        if (settings.color_alert == 0) {
+            settings.color_alert = DEFAULT_COLOR_ALERT;
+        }
     }
 }
 
